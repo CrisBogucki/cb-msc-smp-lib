9c30a45c3e3a060803e396b60280680cbe40b829
Krzysztof Bogucki <krzysztof.lubien@icloud.com>
2021-07-07 15:37:25 +0200

    Update modules.sh

--- modules.sh
+++ modules.sh
@@ -2,8 +2,9 @@ read -r modules<../../modules
 echo $modules
 for mod in $modules
 do
+  clear	
   echo "Generate for $mod module"
-	./generate.sh -m $mod -t cs
+  ./generate.sh -m $mod -t cs
 done
 
 echo " Generate complete!"
